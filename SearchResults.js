3772734d4f352448c665d309e3ab4741c2eb0d7d
Yonatan Abramovich <yonatanabra99@gmail.com>
2023-03-05 13:56:04 +0200

    Milestone 8 done
    it has more effective functionality that in milestone 7 for searching and refreshing results with highlighted color.

--- SearchResults.js
+++ SearchResults.js
@@ -17,7 +17,7 @@ class SearchResult {
         searchList.innerHTML = "";
         func.apply(context, [...args]);
       }, delay);
-    };
+    }; 
   }
   handleSearchClick(event) {
     event.preventDefault();
@@ -46,66 +46,83 @@ class SearchResult {
     }
   }
   listSearchData(data) {
-  const profile = data.profile;
-  let loader = document.getElementById("loader");
-  loader.classList.add("disappear");
-  let changes = profile.changes;
-  const changesNum = changes.toFixed(2);
-
-  const liElement = document.createElement("li");
-  const imgElement = document.createElement("img");
-  const aElement = document.createElement("a");
-  const spanElement = document.createElement("span");
-
-  const inputValue = this.searchInput.value.toLowerCase(); 
-  const companyName = profile.companyName.toLowerCase(); 
-  const symbol = data.symbol.toLowerCase(); 
-
-  const highlightedCompanyName = companyName.replace(inputValue, `<span style="background-color: yellow">${inputValue}</span>`);
-  const highlightedSymbol = symbol.replace(inputValue, `<span style="background-color: yellow">${inputValue}</span>`);
-
-  aElement.innerHTML = ` ${highlightedCompanyName} (${highlightedSymbol})`; 
-  aElement.setAttribute("href", `company.html?symbol=${data.symbol}`);
-  imgElement.setAttribute("src", `${profile.image}`);
-  imgElement.onerror = () => {
-    const defaultImgElement = document.createElement("img");
-    defaultImgElement.setAttribute("src", "download.png");
-    liElement.replaceChild(defaultImgElement, imgElement);
-  };
-  spanElement.textContent = `(${changesNum}%)`;
-
-  if (changesNum >= 0) {
-    spanElement.style.color = "lightgreen";
-  } else {
-    spanElement.style.color = "red";
+    const profile = data.profile;
+    let loader = document.getElementById("loader");
+    loader.classList.add("disappear");
+    let changes = profile.changes;
+    const changesNum = changes.toFixed(2);
+  
+    const liElement = document.createElement("li");
+    const imgElement = document.createElement("img");
+    const aElement = document.createElement("a");
+    const spanElement = document.createElement("span");
+    
+    const compareButtonContainer = document.createElement("div");
+    const compareButton = document.createElement("button");
+    compareButton.textContent = "Compare";
+    compareButton.classList.add("compareBtn");
+    compareButtonContainer.classList.add("compareBtnContainer");
+  
+    const inputValue = this.searchInput.value;
+    const companyName = profile.companyName;
+    const symbol = data.symbol;
+  
+    const regExp = new RegExp(inputValue, "i"); 
+    const highlightedCompanyName = companyName.replace(regExp, `<span style="background-color: yellow">$&</span>`); 
+    const highlightedSymbol = symbol.replace(regExp, `<span style="background-color: yellow">$&</span>`);
+  
+    aElement.innerHTML = ` ${highlightedCompanyName} (${highlightedSymbol})`; 
+    aElement.setAttribute("href", `company.html?symbol=${data.symbol}`);
+    imgElement.setAttribute("src", `${profile.image}`);
+    imgElement.onerror = () => {
+      const defaultImgElement = document.createElement("img");
+      defaultImgElement.setAttribute("src", "download.png");
+      liElement.replaceChild(defaultImgElement, imgElement);
+    };
+    spanElement.textContent = `(${changesNum}%)`;
+  
+    if (changesNum >= 0) {
+      spanElement.style.color = "lightgreen";
+    } else {
+      spanElement.style.color = "red";
+    }
+  
+    compareButton.addEventListener("click", () => {
+      console.log(data); 
+    });
+  
+    compareButtonContainer.appendChild(compareButton); 
+    liElement.append(imgElement, aElement, spanElement, compareButtonContainer); 
+    this.searchList.appendChild(liElement);
   }
-
-  liElement.append(imgElement, aElement, spanElement);
-  this.searchList.appendChild(liElement);
-}
   setupListeners() {
     this.searchInput.addEventListener("input", () => {
-      if (this.searchInput.value === "") {
+      const inputValue = this.searchInput.value;
+      if (inputValue === "") {
         this.searchList.innerHTML = "";
-      }else {
+      } else {
         this.debounce(this.getSearchData.bind(this), 200)();
-      }}); 
-    this.searchInput.addEventListener("keydown", () => {
+      }
+      
       setTimeout(() => {
-        const inputValue = this.searchInput.value.toLowerCase();
+        const inputValue = this.searchInput.value;
         const lis = this.searchList.querySelectorAll("li");
         lis.forEach((li) => {
           const a = li.querySelector("a");
-          const companyName = a.textContent.split(" (")[0].toLowerCase();
-          const symbol = a.textContent.split(")")[0].split("(")[1].toLowerCase();
-          const highlightedCompanyName = companyName.replace(inputValue, `<span style="background-color: yellow">${inputValue}</span>`);
-          const highlightedSymbol = symbol.replace(inputValue, `<span style="background-color: yellow">${inputValue}</span>`);
+          const companyName = a.textContent.split(" (")[0];
+          const symbol = a.textContent.split(")")[0].split("(")[1];
+          const regExp = new RegExp(inputValue, "i");
+          if (!regExp.test(companyName) && !regExp.test(symbol)) {
+            li.remove();
+            return;
+          }
+          
+          const highlightedCompanyName = companyName.replace(regExp, `<span style="background-color: yellow">$&</span>`); 
+          const highlightedSymbol = symbol.replace(regExp, `<span style="background-color: yellow">$&</span>`);
           a.innerHTML = ` ${highlightedCompanyName} (${highlightedSymbol})`;
         });
       }, 0);
     });
-
     this.searchButton.addEventListener("click", this.handleSearchClick.bind(this));
   }
 }
-
